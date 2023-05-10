ecc9dfb7b66ed04420c3fb7118cc95c8f971e88f
Brian Schlining <bschlining@gmail.com>
2023-05-10 11:37:24 -0700

    Starting #1

---
+++ mondrian/src/main/java/org/mbari/mondrian/msg/commands/DeleteAnnotationsCmd.java
@@ -0,0 +1,21 @@
+package org.mbari.mondrian.msg.commands;
+
+import org.mbari.mondrian.ToolBox;
+
+public class DeleteAnnotationsCmd implements Command {
+
+    @Override
+    public void apply(ToolBox toolBox) {
+
+    }
+
+    @Override
+    public void unapply(ToolBox toolBox) {
+
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
---
+++ mondrian/src/main/java/org/mbari/mondrian/msg/commands/DeleteLocalizationsCmd.java
@@ -0,0 +1,20 @@
+package org.mbari.mondrian.msg.commands;
+
+import org.mbari.mondrian.ToolBox;
+
+public class DeleteLocalizationsCmd implements Command {
+    @Override
+    public void apply(ToolBox toolBox) {
+
+    }
+
+    @Override
+    public void unapply(ToolBox toolBox) {
+
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
